7f034fa2f2dd9e2c25de1efa25b3c8f11ef02f25
Yiqing Ren <yiqing.1.ren@kcl.ac.uk>
2024-02-12 09:03:36 +0000

    fix error

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
 	name = models.CharField(unique = True, blank = False, max_length=30)
-	description = models.TextField(blank = True, max_length = 120)
+	description = models.CharField(blank = True, max_length = 120)
 	quantity = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
 
 	def __str__(self):
